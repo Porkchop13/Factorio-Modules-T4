c023050a58e9fce1f244cf0acc8e4c4fca09e0af
Andrew <andrew.weinmann@gmail.com>
2024-12-28 20:18:21 -0600

    feat: Add Space Age support

--- prototypes/productivity.lua
+++ prototypes/productivity.lua
@@ -16,14 +16,30 @@ table.insert(container, item)
 data:extend(container)
 
 -- [Recipe] --
+local recipeCategory
+local recipeIngredients
+
+if mods["space-age"] then
+    recipeCategory = "electronics"
+    recipeIngredients = {
+        {type= "item", name="productivity-module-3", amount=5},
+        {type= "item", name="quantum-processor", amount=5},
+        {type= "item", name="biter-egg", amount=1}
+    }
+else
+    recipeCategory = "crafting"
+    recipeIngredients = {
+        {type= "item", name="productivity-module-3", amount=5},
+        {type= "item", name="processing-unit", amount=10}
+    }
+end
+
 data:extend({
     {
+        category = recipeCategory,
         enabled = false,
         energy_required = 120,
-        ingredients = {
-            {type= "item", name="productivity-module-3", amount=5},
-            {type= "item", name="processing-unit", amount=10}
-        },
+       ingredients = recipeIngredients,
         name = "productivity-module-4",
         results = {{type= "item", name="productivity-module-4",amount=1}},
         type = "recipe"
@@ -31,21 +47,43 @@ data:extend({
 })
 
 -- [Technology] --
+local technologyCount
+local technologyIngredients
+local technologyPrerequisites
+
+if mods["space-age"] then
+    technologyCount = 500
+    technologyIngredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"space-science-pack", 1},
+        {"agricultural-science-pack", 1},
+        {"cryogenic-science-pack", 1}
+    }
+    technologyPrerequisites = {"productivity-module-3", "cryogenic-science-pack"}
+else
+    technologyCount = 500
+    technologyIngredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 1}
+    }
+    technologyPrerequisites = {"productivity-module-3"}
+end
+
 data:extend({
     {
         effects = {{type = "unlock-recipe", recipe = "productivity-module-4"}},
         icon = "__modules-t4__/graphics/technology/productivity-module-4.png",
         icon_size = 256,
         name = "productivity-module-4",
-        prerequisites = {"productivity-module-3"},
+        prerequisites = technologyPrerequisites,
         type = "technology",
         unit = {
-            count = 500,
-            ingredients = {
-                {"automation-science-pack", 1}, {"logistic-science-pack", 1},
-                {"chemical-science-pack", 1}, {"production-science-pack", 1}
-
-            },
+            count = technologyCount,
+            ingredients = technologyIngredients,
             time = 60
         },
         upgrade = true
